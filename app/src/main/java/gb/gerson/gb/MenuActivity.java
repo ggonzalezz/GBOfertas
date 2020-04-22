ed055bc50cded8defab7c5845cb34c1730876097
gerson7777 <gersonbreakgonzalez@gmail.com>
2020-04-21 20:02:50 -0600

    Agregando menu a nuestro proyecto

--- app/src/main/java/gb/gerson/gb/MenuActivity.java
+++ app/src/main/java/gb/gerson/gb/MenuActivity.java
@@ -1,14 +1,40 @@
 package gb.gerson.gb;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.ramotion.circlemenu.CircleMenuView;
 public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.txt_titulo);
+        final CircleMenuView circleMenuView = (CircleMenuView)findViewById(R.id.circle_menu);
+        circleMenuView.setEventListener(new CircleMenuView.EventListener()
+        {
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int buttonIndex) {
+                if (buttonIndex==0){
+                    Toast.makeText(MenuActivity.this, "yi", Toast.LENGTH_SHORT).show();
+                }
+                if (buttonIndex==1){
+                    Toast.makeText(MenuActivity.this, "Hola", Toast.LENGTH_SHORT).show();
+                }
+                if (buttonIndex==2){
+                    Toast.makeText(MenuActivity.this, "Hola1", Toast.LENGTH_SHORT).show();
+                }
+                if (buttonIndex==3){
+                    Toast.makeText(MenuActivity.this, "Hola2", Toast.LENGTH_SHORT).show();
+                }
+                //super.onButtonClickAnimationStart(view, buttonIndex);
+            }
+        });
     }
 }
